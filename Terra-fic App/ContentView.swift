fbb5d3f844b6874eda2a679295c1cbb24777d728
Elliott Yu <elliottyu1@gmail.com>
2023-03-30 15:51:07 +0100

    Initial Commit

---
+++ Terra-fic App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Terra-fic App
+//
+//  Created by Elliott Yu on 30/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Terra-fic App/Terra_fic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Terra_fic_AppApp.swift
+//  Terra-fic App
+//
+//  Created by Elliott Yu on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Terra_fic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
